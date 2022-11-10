5d64209fd42b8fec43d1fcbc804e34e2f0173e03
Astambar <88654523+Astambar@users.noreply.github.com>
2022-11-10 13:50:55 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,59 +1,84 @@
-<?php
-session_start();
-include './structurepage/header.php';
-$bdd = new PDO('mysql:host=localhost;dbname=messages_prives;charset=utf8;','root','');
-$bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-if(!$_SESSION['mdp'])
-{
-	header('location: default_page.php');
-}
-if(!$_SESSION['pseudo'])
-{
-	header('location: default_page.php');
-}
-$bdd = new PDO('mysql:host=localhost;dbname=messages_prives;charset=utf8;','root','');
-$bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-if(isset($_POST['valider']))
-{
-	if(!empty($_POST['message']))
-	{
-		$message = nl2br(htmlspecialchars($_POST['message']));
-		$idSession = $_SESSION['id'];
-		$insereMessage =$bdd->prepare('INSERT INTO chat_general(id_auteur, message) VALUES(?, ?)');
-		$insereMessage->execute(array($idSession, $message));
-		header('Location: ');
-	}
-	else
-	{
-		echo "message vide";
-	}
-}
-
-?>
-
-	<div class="container">
-		<div id="messages" class="body"></div>
-		<div class="footer">
-			<form method="POST" action="">
-						<input type="text" name="message"/>
-						<input type="submit" name="valider" value="SEND">
-			</form>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>TchatEvolution</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<header>
+		<nav class="nav-header">
+			<ul>
+				<li>
+					<a href="TchatEvolution.mp4"><img src="image/logo.png" alt="logo redirect video TchatEvolution Demo"></a>
+				</li>
+				<li>
+					<a href="#cover-image">Cover</a>
+				</li>
+				<li>
+					<a href="#demo">Demo</a>
+				</li>
+				<li>
+					<a href="#features">Features</a>
+				</li>
+				<li>
+					<a href="#about">À propos</a>
+				</li>
+				<li>
+					<a href="#title">Titre</a>
+				</li>
+			</ul>
+		</nav>
+		<div class="cover-image">
+			<img id="cover-image" src="image/banner-test1.png" alt="TchatEvolution General Full Screen">
 		</div>
-	</div>
-
-	<script>
-		const menuHamburger = document.querySelector(".menu-hamburger")
-		const navLinks = document.querySelector(".nav-links")
-
-		menuHamburger.addEventListener('click',()=>{
-			navLinks.classList.toggle('mobile-menu')
-		})
-		setInterval((load_message) => {
-			$('#messages').load('dynamiquePage/loadMessage.php');
-			var messageBody = document.querySelector('#messages');
-			messageBody.scrollTop = messageBody.scrollHeight - messageBody.clientHeight;
-		}, 500);
-	</script>
+		<h1 id="title">TchatEvolution</h1>
+		<h3>TchatEvolution le tchat qui évolue avec sa communauté et son époque</h3>
+	</header>
+	<main>
+		<section id="features">
+			<article>
+				<img src="image/defaultPage.png" alt="TchatEvolution Représente la page par default">
+				<section>
+					<h1>Page par default</h1>
+					<p>Cette page s'affiche quand un utilisateur n'est pas connecté à fin de lui permettre d'accéder au service de tchat soit en s'inscrivant soit en se connectant.</p>
+				</section>
+			</article>
+			<article>
+				<img src="image/connectionPage.png" alt="TchatEvolution Représente la page de connection">
+				<section>
+					<h1>Connection</h1>
+					<p>Cette fonctionnalité permet de se connecter afin de pouvoir utiliser ce service de tchat.</p>
+				</section>
+			</article>
+			<article>
+				<img src="image/inscriptionPage.png" alt="TchatEvolution Représente la page d'inscription">
+				<section>
+					<h1>Inscription</h1>
+					<p>Cette fonctionnalité permet de s'inscrire afin de pouvoir utiliser ce service de tchat.</p>
+				</section>
+			</article>
+			<article>
+				<img src="image/tchatGeneralPage.png" alt="TchatEvolution Représente la page de tchat general">
+				<section>
+					<h1>Tchat General</h1>
+					<p>Cette fonctionnalité permet de discuter avec l'ensemble des utilisateurs.</p>
+				</section>
+			</article>
+		</section>
+		<section id="about">
+			<h1>À propos</h1>
+			<p>Me Présenter</p>
+			<p>
+				Je suis un grand utilisateur de tchat et de diverse application et service de rencontre, c'est pourquoi après avoir vu autant de diversité termes de fonctionnalité, je me suis dit que cela pourrait être intéressant à regrouper dans le futur les différentes fonctionnalités que j'ai pu trouver cool en un seul endroit et en ajouter de nouvelle.
+				Et puis c'est quand même vachement cool de posséder son propre moteur de tchat n'est-ce pas ?
+				Voilà pourquoi j'ai commencer à mettre en place un nouveau moteur de tchat.
+			</p>
+			<p>
+			</p>
+		</section>
+	</main>
 </body>
 </html>
